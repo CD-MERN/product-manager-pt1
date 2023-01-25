4ef3b03ce3b5b4cbad029e9efbbb99a80391190c
Federico Baez <federicko94@gmail.com>
2023-01-24 22:24:07 -0300

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Form from './components/Form';
+
+function App() {
+  return (
+    <div className="App w-50 mx-auto">
+      <Form />
+    </div>
+  );
+}
+
+export default App;
